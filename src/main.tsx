1574777b09c4910b335d1ad42e9c0cabff891a47
Roger Collado <roger.collado1130@gmail.com>
2025-02-12 17:24:31 +0100

    add translations i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
+import './18n.js';
 import App from './App.tsx';
+import { BlogPage } from './pages/blog.tsx';
+import { TeamsPage } from './pages/teams.tsx';
 import ErrorPage from './pages/error-page.tsx';
 
 import '@scss/main.scss';
@@ -12,6 +14,14 @@ const router = createBrowserRouter([
 		path: '/',
 		element: <App />,
 		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/blog',
+		element: <BlogPage />,
+	},
+	{
+		path: '/teams',
+		element: <TeamsPage />,
 	},
 ]);
 
