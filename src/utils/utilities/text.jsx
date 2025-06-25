7d4a117b101e8ce7bb1856dc3eb3dcedbeb69ba8
Roger Collado <roger.collado1130@gmail.com>
2025-06-25 09:53:42 +0900

    split blog paragraphs

--- src/utils/utilities/text.jsx
+++ src/utils/utilities/text.jsx
@@ -1,3 +1,15 @@
 export const truncateText = (text, maxLength) => {
 	return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
 };
+
+export default function paragraphBlock({ text }) {
+	return (
+		<>
+			{text.split(/\n{2,}/).map((paragraph, idx) => (
+				<p key={idx} className="text-lg text-gray-800 leading-relaxed mb-6">
+					{paragraph.trim()}
+				</p>
+			))}
+		</>
+	);
+}
