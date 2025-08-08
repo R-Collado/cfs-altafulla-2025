b158dcbbda05ab94d0c8182f656eb43c98851f85
Roger Collado <roger.collado1130@gmail.com>
2025-08-08 16:13:51 +0900

    add logo and fix styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router';
 import { PrimaryNav } from './primary-nav';
 import { useNavbarScrollAnimation } from '@/utils/utilities/scroll';
 import { LanguageSelector } from './language-selector';
+import LogoIcon from '@/assets/logos/team-logo.svg?react';
 // import { hideNavBar, showNavBar } from '@/animations/navbar';
 
 export const PrimaryHeader = () => {
@@ -10,9 +11,11 @@ export const PrimaryHeader = () => {
 	useNavbarScrollAnimation(); // 👈 apply the custom hook
 
 	return (
-		<header className="primary-header | flex space-between fixed">
+		<header className="primary-header | flex space-between align-center fixed">
 			<div className="flex align-center">
-				<Link to="/">Club Futsal Altafulla</Link>
+				<Link to="/">
+					<LogoIcon className="team-logo" />
+				</Link>
 			</div>
 			<PrimaryNav />
 			<LanguageSelector />
