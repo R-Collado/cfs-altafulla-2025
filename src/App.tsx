30f4942eabd27d034c7d23461586a1cb567a47b6
Roger Collado <roger.collado1130@gmail.com>
2025-09-04 11:18:19 +0900

    add lenis

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,24 @@
 import { Suspense } from 'react';
 import './App.css';
+import Lenis from 'lenis';
 
 import { PrimaryHeader } from './components/header';
-import { useTranslation } from 'react-i18next';
 import { Outlet } from 'react-router-dom';
 
-// const dummyPost: PostPreviewProps = {
-// 	title: 'El equipo ha vuelto a ganar en casa después de una mala racha colectiva',
-// 	excerpt: 'Discover the top 5 futsal techniques that every player needs to know to dominate the court.',
-// 	image: '/images/blog/dummy-post.png',
-// 	tag: 'Training',
-// 	date: '2025-02-10T14:30:00Z', // ISO 8601 format
-// 	type: 'home',
-// };
-
-// const dummyPlayer: FieldPlayer = {
-// 	id: 2,
-// 	firstName: 'Alejandro',
-// 	lastName: 'Balde',
-// 	role: 'keeper',
-// 	team: 'FC Barcelona',
-// 	birthDay: '2004-10-18',
-// 	birthPlace: 'Barcelona',
-// 	isCurrentPlayer: true,
-// 	number: 3,
-// 	nickname: 'Ale',
-// 	careerStats: {
-// 		matches: 100,
-// 		goals: 20,
-// 		saves: 10,
-// 	},
-// 	seasonStats: {
-// 		matches: 20,
-// 		goals: 5,
-// 		saves: 5,
-// 	},
-// };
-
 function App() {
-	const { t } = useTranslation();
-	// const playersLink = `${baseUrl}players`;
+	// Initialize Lenis
+	const lenis = new Lenis({
+		autoRaf: true,
+	});
 
+	// Listen for the scroll event and log the event data
+	// lenis.on('scroll', (e) => {
+	// 	console.log(e);
+	// });
 	return (
 		<Suspense fallback={<div>hello world</div>}>
 			<PrimaryHeader />
 			<Outlet />
-
-			{/* <PlayerCard player={dummyPlayer}></PlayerCard> */}
-			{/* <PlayerCard player={dummyPost}></
-		// 	}
-		// 	{/* <PlayerCard player={players[0]}></PlayerCard> */}
 		</Suspense>
 	);
 }
