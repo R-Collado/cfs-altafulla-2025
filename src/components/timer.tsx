67dfcd02d473269f71d499a0e19191eebe1c857d
Roger Collado <roger.collado1130@gmail.com>
2025-09-09 05:44:37 +0900

    show next match  at timer automatically

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,44 +1,49 @@
-import { useEffect, useRef } from 'react';
+import { API_BASE_URL } from '@/utils/utilities/config';
+import { useEffect, useRef, useState } from 'react';
+import dayjs from 'dayjs';
 
-const targetDate = new Date('September 7, 2025 03:00:00');
+const matchesUrl = API_BASE_URL + '/matches';
 
 export const Timer = () => {
+	const [nextMatch, setNextMatch] = useState(null);
+
 	const timerRef = useRef<HTMLParagraphElement>(null);
 
-	const starterTime = updateCountdown();
-
 	useEffect(() => {
-		setInterval(() => {
-			timerRef.current!.innerText = updateCountdown();
-		}, 1000);
+		fetch(matchesUrl + '/next')
+			.then((res) => res.json()) // ← return the parsed JSON
+			.then((data) => {
+				setNextMatch(data);
+				setInterval(() => {
+					const matchDate = dayjs(data.date + ' ' + data.time);
+					timerRef.current!.innerText = updateCountdown(matchDate);
+				}, 1000);
+			});
 	}, []);
 
-	const matchDay = targetDate.toLocaleDateString('es', {
-		day: 'numeric',
-		month: 'long',
-		year: 'numeric',
-	});
+	const matchDay = dayjs(nextMatch?.date + ' ' + nextMatch?.time);
+	console.log(matchDay, nextMatch?.rival);
 
 	return (
 		<div className="next-match-timer | text-left">
-			<p ref={timerRef} className="time">
-				{starterTime}
-			</p>
-			<p className="date">{matchDay}</p>
+			<p ref={timerRef} className="time"></p>
+			<p className="date">{matchDay.format('DD [de] MMMM [de] YYYY')}</p>
 		</div>
 	);
 };
 
-const updateCountdown = () => {
-	const now = new Date();
-	const diff = targetDate.getTime() - now.getTime();
+const updateCountdown = (targetDate: any) => {
+	const now = dayjs();
+	const diff = dayjs(targetDate).diff(now);
+	const date = dayjs(targetDate).toString();
+	console.log(date);
 
-	const days = String(Math.floor(diff / (1000 * 60 * 60 * 24))).padStart(2, '0');
-	const hours = String(Math.floor((diff / (1000 * 60 * 60)) % 24)).padStart(2, '0');
-	const minutes = String(Math.floor((diff / (1000 * 60)) % 60)).padStart(2, '0');
-	const seconds = String(Math.floor((diff / 1000) % 60)).padStart(2, '0');
+	const days = dayjs(diff).date() + 1;
+	const minutes = dayjs(diff).minute();
+	const hours = dayjs(diff).hour();
+	const seconds = dayjs(diff).second();
 
-	if (days < '01') {
+	if (days < 1) {
 		return `${hours} : ${minutes} : ${seconds}`;
 	}
 
